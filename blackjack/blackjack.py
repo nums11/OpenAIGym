4b5997656066c8b4d92203b0633a5c0fea39bb30
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2021-04-05 00:00:05 -0400

    Changes

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -2,8 +2,19 @@ import gym
 import numpy as np
 from tqdm import tqdm
 import random
-from simple_blackjack_agent import SimpleBlackjackAgent
-from advanced_blackjack_agent import AdvancedBlackjackAgent
+from simple_q_learning_agent import SimpleQLearningAgent
+from advanced_q_learning_agent import AdvancedBlackjackAgent
+from simple_monte_carlo_agent import SimpleMonteCarloAgent
+from advanced_monte_carlo_agent import AdvancedMonteCarloAgent
+
+def comparePolicies(p1, p2):
+  print('State        P1 Action   P2 Action   Different?')
+  for state, action in p1.items():
+    diff_string = ''
+    if action != p2[state]:
+      diff_string = '        DIFFERENT'
+    print(f'{state}:    {action}            {p2[state]}'
+      + diff_string)
 
 # Actions: 0 - stick, 1 - hit
 # Observation: 
@@ -16,22 +27,47 @@ state_space_size = 2
 action_space_size = 2
 num_episodes = 1000000
 
-# simple_agent = SimpleBlackjackAgent()
-# simple_agent.train(env, num_episodes)
+simple_ql_agent = SimpleQLearningAgent()
+# simple_ql_agent.train(env, num_episodes)
 
 # advanced_agent = AdvancedBlackjackAgent()
 # advanced_agent.train(env, num_episodes)
 
-rewards = []
-with open('rewards') as f:
-  rewards = f.read().splitlines()
-rewards = np.array(rewards)
-rewards = [float(i) for i in rewards]
-split = 100000  # 100k
-rewards_per_split_episodes = np.split(np.array(rewards),num_episodes/split)
-count = split
+simple_monte_carlo_agent = SimpleMonteCarloAgent()
+# simple_monte_carlo_agent.train(env, num_episodes)
 
-print(f"\n********Average reward per {split} episodes ********\n")
-for r in rewards_per_split_episodes:
-  print(count, ": ", str(sum(r/split)))
-  count += split
+advanced_monte_carlo_agent = AdvancedMonteCarloAgent()
+# advanced_monte_carlo_agent.train(env,num_episodes)
+
+simple_mc_policy_100 = np.load('simple_mc_policy_100.npy',allow_pickle='TRUE').item()
+simple_mc_policy_1k = np.load('simple_mc_policy_1k.npy',allow_pickle='TRUE').item()
+simple_mc_policy_10k = np.load('simple_mc_policy_10k.npy',allow_pickle='TRUE').item()
+simple_mc_policy_100k = np.load('simple_mc_policy_100k.npy',allow_pickle='TRUE').item()
+simple_mc_policy_1mil = np.load('simple_mc_policy_1mil.npy',allow_pickle='TRUE').item()
+advanced_mc_policy_100 = np.load('advanced_mc_policy_100.npy',allow_pickle='TRUE').item()
+advanced_mc_policy_1k = np.load('advanced_mc_policy_1k.npy',allow_pickle='TRUE').item()
+advanced_mc_policy_10k = np.load('advanced_mc_policy_10k.npy',allow_pickle='TRUE').item()
+advanced_mc_policy_100k = np.load('advanced_mc_policy_100k.npy',allow_pickle='TRUE').item()
+advanced_mc_policy_1mil = np.load('advanced_mc_policy_1mil.npy',allow_pickle='TRUE').item()
+
+# simple_ql_policy = np.load('simple_ql_policy.npy',allow_pickle='TRUE').item()
+
+# comparePolicies(simple_mc_policy_1k, simple_mc_policy_1mil)
+
+# simple_ql_agent.test(env, 100000, simple_mc_policy_1mil)
+simple_monte_carlo_agent.test(env, 1000000, simple_mc_policy_1mil)
+# advanced_monte_carlo_agent.test(env, 1000000, advanced_mc_policy_1k)
+
+# rewards = []
+# with open('advanced_rewards') as f:
+#   rewards = f.read().splitlines()
+# rewards = np.array(rewards)
+# rewards = [float(i) for i in rewards]
+# split = 100000
+# rewards_per_split_episodes = np.split(np.array(rewards),num_episodes/split)
+# count = split
+
+# print(f"\n********Average reward per {split} episodes ********\n")
+# for r in rewards_per_split_episodes:
+#   print(count, ": ", str(sum(r/split)))
+#   count += split
---
+++ blackjack/simple_monte_carlo_agent.py
@@ -0,0 +1,138 @@
+import numpy as np
+from tqdm import tqdm
+import random
+import matplotlib.pyplot as plt
+
+"""
+State-space is just the player current sum and
+whether or not they have an ace
+State-space size: 42
+"""
+class SimpleMonteCarloAgent:
+  def train(self, env, num_episodes):
+    state_space_size = 2
+    action_space_size = 2
+    # learning_rate = 0.1
+    # learning_rate = 0.05
+    learning_rate = 0.01
+    rewards_all_episodes = []
+    # discount_rate = 0.99
+    discount_rate = 1
+    q_table = {}
+    bool = False
+    for i in range(1,22):
+      q_table[(i,bool)] = np.zeros((action_space_size))
+      bool = not bool
+      q_table[(i,bool)] = np.zeros((action_space_size))
+      bool = not bool
+
+    # Holds the returns for every state action pair across all episodes
+    returns = {}
+    # Holds the deltas for every state action pair across all episodes
+    deltas = {}
+    for key in q_table.keys():
+      for action in range(2):
+        returns[(key,action)] = list()
+        deltas[(key,action)] = list()
+
+    for _ in tqdm(range(num_episodes)):
+      episode = self.generateBlackJackEpisode(env)
+      G = 0
+      for i, step in enumerate(episode[::-1]): # reverse the list
+        # Every step is represented as an array with 4 elements:
+        # initial state, action, reward, new_state
+        G = discount_rate * G + step[2]
+        initial_state = step[0]
+        action = step[1]
+        # If this step was not already visited in the episode
+        if initial_state not in [x[0] for x in episode[::-1][len(episode)-i:]]:
+          returns[(initial_state, action)].append(G)
+          new_avg_state_action_value = np.average(returns[(initial_state, action)])
+          delta = np.abs(q_table[initial_state][action] - new_avg_state_action_value)
+          deltas[(initial_state, action)].append(delta)
+          q_table[initial_state][action] = new_avg_state_action_value
+
+    policy = self.getPolicyFromQTable(q_table)
+    self.savePolicy(policy)
+    # Plot change in deltas over iterations
+    plt.figure(figsize=(20,10))
+    all_series = [list(x)[:50] for x in deltas.values()]
+    for series in all_series:
+      plt.plot(series)
+    plt.show()
+
+  def generateBlackJackEpisode(self, env):
+    player_sum, dealer_sum, has_ace = env.reset()
+    state = (player_sum, has_ace)
+    # 2-d array where each element is a step in the episode
+    # represented as an array with 4 elements:
+    # initial state, action, reward, new_state
+    episode = []
+    done = False
+
+    while True:
+      if done:
+        return episode
+      action = self.getRandomAction()
+      observation, reward, done, info = env.step(action)
+      player_sum, dealer_sum, has_ace = observation
+      new_state = (player_sum,has_ace)
+      episode.append([state, action, reward, new_state])
+      state = new_state
+
+  # Return random int between 0 and 1 (inclusive)
+  def getRandomAction(self):
+    return random.randint(0,1)
+
+  def printRewards(self, num_episodes, rewards):
+    split = 100
+    rewards_per_split_episodes = np.split(np.array(rewards),num_episodes/split)
+    count = split
+
+    print(f"\n********Average reward per {split} episodes ********\n")
+    for r in rewards_per_split_episodes:
+      print(count, ": ", str(sum(r/split)))
+      count += split
+
+  def saveRewards(self, rewards):
+    with open('simple_rewards', 'w') as file_handler:
+      for item in rewards:
+        file_handler.write("{}\n".format(item))
+
+  def getPolicyFromQTable(self, q_table):
+    policy = {key:np.argmax(action_values) for key, action_values in q_table.items()}
+    return policy
+
+  def savePolicy(self, policy):
+    np.save('policy.npy', policy)
+
+  def test(self, env, num_episodes, policy):
+    env.reset()
+    done = False
+    rewards_all_episodes = []
+
+    num_skipped = 0
+    for episode in tqdm(range(num_episodes)):
+      player_sum, dealer_sum, has_ace = env.reset()
+      # Special case - when you auto win
+      if player_sum == 21:
+        num_skipped += 1
+        continue
+
+      state = (player_sum, has_ace)
+      done = False
+      while True:
+        if done:
+          rewards_all_episodes.append(reward)
+          break
+        action = policy[state]
+        observation, reward, done, info = env.step(action)
+        player_sum, dealer_sum, has_ace = observation
+        new_state = (player_sum,has_ace)
+        state = new_state
+
+    avg_rewards = sum(rewards_all_episodes) / num_episodes
+    print(f'******* Average rewards across {num_episodes} episodes *******')
+    print(avg_rewards)
+
+
